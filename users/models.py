a85f09aa1af6133b65956ac13f5f67ce85907116
panthurr0 <olapopaola@mail.ru>
2024-06-12 14:11:58 +0300

    add fixtures

--- users/models.py
+++ users/models.py
@@ -1,15 +1,32 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import Course, Lesson
+
 NULLABLE = {'blank': True, 'null': True}
 
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(verbose_name='почта', unique=True)
-    phone_number = models.CharField(verbose_name='номер телефона', max_length=30, **NULLABLE)
-    avatar = models.ImageField(verbose_name='аватар', upload_to='users/', **NULLABLE)
-    city = models.CharField(verbose_name='город', max_length=100, **NULLABLE)
+    email = models.EmailField(
+        verbose_name='почта',
+        unique=True
+    )
+    phone_number = models.CharField(
+        verbose_name='номер телефона',
+        max_length=30,
+        **NULLABLE
+    )
+    avatar = models.ImageField(
+        verbose_name='аватар',
+        upload_to='users/',
+        **NULLABLE
+    )
+    city = models.CharField(
+        verbose_name='город',
+        max_length=100,
+        **NULLABLE
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -20,3 +37,41 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Payment(models.Model):
+    TRANSFER = 'Перевод'
+    CASH = 'Наличные'
+
+    PAYMENT_METHODS = [
+        (TRANSFER, 'Перевод на счёт'),
+        (CASH, 'Наличными'),
+    ]
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        **NULLABLE
+    )
+    pay_date = models.DateField(
+        verbose_name='Дата оплаты',
+        **NULLABLE
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        **NULLABLE
+    )
+    lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE,
+        **NULLABLE
+    )
+    pay_sum = models.PositiveIntegerField(
+        verbose_name='Сумма оплаты'
+    )
+    pay_method = models.CharField(
+        verbose_name='Способ оплаты',
+        choices=PAYMENT_METHODS,
+        default=TRANSFER
+        )
