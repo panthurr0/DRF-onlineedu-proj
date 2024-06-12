739f6043ce8951d3ad2631907f07e63eb37ca42a
panthurr0 <olapopaola@mail.ru>
2024-06-12 12:34:20 +0300

    add lesson_count to courseserializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -4,9 +4,15 @@ from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
-        fields = ('id', 'course_title', 'course_description')
+        fields = ('id', 'course_title', 'course_description',
+                  'lesson_count')
+
+    def get_lesson_count(self, instance):
+        return Lesson.objects.filter(course=instance).count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
