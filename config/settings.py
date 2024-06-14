a8a09693a5a28ca677e8835aaff95f778a052b46
panthurr0 <olapopaola@mail.ru>
2024-06-14 21:25:38 +0300

    add jwt

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'django_filters',
     'rest_framework',
+    'rest_framework_simplejwt',
 
     'users',
     'materials',
