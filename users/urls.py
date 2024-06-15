a0b3037f89a9c005c522f2ed237b6091e2b097eb
panthurr0 <olapopaola@mail.ru>
2024-06-15 20:01:25 +0300

    add CRUD for users

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentListAPIView, UserCreateAPIView
+from users.views import (PaymentListAPIView, UserCreateAPIView, UserListAPIView, UserUpdateAPIView,
+                         UserRetrieveAPIView, UserDestroyAPIView)
 
 app_name = UsersConfig.name
 
@@ -11,5 +12,9 @@ urlpatterns = [
     path('payment/', PaymentListAPIView.as_view(), name='payment-list'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
+    path('list/', UserListAPIView.as_view(), name='list'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='update'),
+    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='retrieve'),
+    path('destroy/<int:pk>/', UserDestroyAPIView.as_view(), name='destroy'),
     path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,12 @@
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
-from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.generics import (CreateAPIView, RetrieveAPIView,
+                                     DestroyAPIView, ListAPIView, UpdateAPIView)
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from users.models import Payment, User
-from users.serializers import UserSerializer
-from users.serilalizers import PaymentSerializer
+from users.serializers import UserSerializer, PaymentSerializer
 
 
 class PaymentListAPIView(generics.ListAPIView):
@@ -26,3 +26,28 @@ class UserCreateAPIView(CreateAPIView):
         user = serializer.save(is_active=True)
         user.set_password(user.password)
         user.save()
+
+
+class UserListAPIView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    filterset_fields = ('email',)
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
