27c0e539f3c71678932187e90f068f2480ab3b4b
panthurr0 <olapopaola@mail.ru>
2024-06-15 18:50:10 +0300

    add authorization with jwt token

--- users/urls.py
+++ users/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('payment/', PaymentListAPIView.as_view(), name='payment-list'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
 ]
