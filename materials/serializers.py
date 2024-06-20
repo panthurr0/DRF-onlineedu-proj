07357ceb7ba8ae5e19f94ff98a9b09197e67d7ca
panthurr0 <olapopaola@mail.ru>
2024-06-20 19:42:05 +0300

    add is_subscribed to CourseSerializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from materials.models import Course, Lesson
 from materials.validators import VideoURLValidator
+from users.models import Subscription
 
 
 class LessonSerializer(ModelSerializer):
@@ -12,10 +13,21 @@ class LessonSerializer(ModelSerializer):
 
 class CourseSerializer(ModelSerializer):
     lesson_count = SerializerMethodField()
+    is_subscribed = SerializerMethodField()
     lesson = LessonSerializer(many=True, read_only=True)
 
     def get_lesson_count(self, instance):
+        """ Считает количество уроков на курсе """
+
         return Lesson.objects.filter(course=instance).count()
+
+    def get_is_subscribed(self, instance):
+        """ Проверяет наличие подписки на курс у текущего пользователя"""
+
+        if Subscription.objects.filter(course=instance).exists():
+            return f'Подписка присутствует'
+        else:
+            return f'Подписка отсутствует'
 
     class Meta:
         model = Course
